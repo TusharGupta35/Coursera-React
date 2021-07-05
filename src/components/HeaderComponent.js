3319770cb2c32a83ccdbfb76da0bdb545ea8610d
Tushar Gupta <tushartafs2012@gmail.com>
2021-07-05 16:54:47 +0530

    Combining Reducers

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -59,7 +59,7 @@ class Header extends Component {
                                     <NavLink className='nav-link' to='/contactus'><span className='fa fa-address-card fa-lg'></span> Contact Us</NavLink>
                                 </NavItem>
                             </Nav>
-                            <Nav className="ml-auto" navbar>
+                            <Nav navbar>
                                 <NavItem>
                                     <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in"></span> Login</Button>
                                 </NavItem>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { createStore, combineReducers } from "redux";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        })
     );
     
     return store;
