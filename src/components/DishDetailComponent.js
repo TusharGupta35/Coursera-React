56d62011e147f8476a63edebbaaf58f60ee6fcf4
Tushar Gupta <tushartafs2012@gmail.com>
2021-07-14 16:36:18 +0530

    React Animations

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -64,7 +64,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         alert('Comment Added Successfully');
-        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.name, values.comment);
     }
 
     render() {
